663aa3d9015c78846f57a452ef42ba4a568119f5
Mi-kio <sakshihere34@gmail.com>
2022-04-13 23:17:36 +0530

    first challenge

--- app.js
+++ app.js
@@ -31,3 +31,10 @@ app.use(express.static("public"));
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+
+
+
+app.get("/",function(req,res){
+  res.render(__dirname + '/views/home.ejs');
+})
